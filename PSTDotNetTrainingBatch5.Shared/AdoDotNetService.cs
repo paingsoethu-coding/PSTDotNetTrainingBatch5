a2f5e14b868e6cd16c9886a90efe2bdc9ac366a6
Paing Soe Thu <whoistheblack@gmail.com>
2025-01-29 23:40:08 +0700

    feat: AdoDotNetService, DapperService

---
+++ PSTDotNetTrainingBatch5.Shared/AdoDotNetService.cs
@@ -0,0 +1,79 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace PSTDotNetTrainingBatch5.Shared
+{
+    public class AdoDotNetService
+    {
+        private readonly string _connectionString;
+
+        public AdoDotNetService(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public DataTable Query(string query, params SqlParametersModel[] sqlParameters)
+        {
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+
+            if (sqlParameters is not null)
+            {
+                foreach (var sqlParameter in sqlParameters)
+                {
+                    cmd.Parameters.AddWithValue(sqlParameter.Name, sqlParameter.Value);
+                }
+            }
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            connection.Close();
+
+            return dt;
+        }
+
+        public int Excute(string query, params SqlParametersModel[] sqlParameters)
+        {
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+
+            if (sqlParameters is not null)
+            {
+                foreach (var sqlParameter in sqlParameters)
+                {
+                    cmd.Parameters.AddWithValue(sqlParameter.Name, sqlParameter.Value);
+                }
+            }
+
+            var result = cmd.ExecuteNonQuery();
+
+            connection.Close();
+
+            return result;
+        }
+    }
+
+    public class SqlParametersModel
+    {
+        public string Name { get; set; }
+
+        public object Value { get; set; }
+
+        public SqlParametersModel() { }
+
+        public SqlParametersModel(string name, object value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+
+
+
+}
---
+++ PSTDotNetTrainingBatch5.Shared/DapperService.cs
@@ -0,0 +1,52 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSTDotNetTrainingBatch5.Shared
+{
+    public class DapperService
+    {
+        private readonly string _connectionString;
+        private readonly IDbConnection _db;
+
+        public DapperService(string connectionString)
+        {
+            _connectionString = connectionString;
+
+            _db = new SqlConnection(_connectionString);
+        }
+
+        public List<T> Query<T>(string query, object? param = null)
+        {
+            //using IDbConnection db = new SqlConnection(_connectionString);
+            var lst = _db.Query<T>(query, param).ToList();
+
+            return lst;
+        }
+
+        public T QueryFirstOrDefault<T>(string query, object? param = null)
+        {
+            //using IDbConnection db = new SqlConnection(_connectionString);
+            var item = _db.QueryFirstOrDefault<T>(query, param);
+
+            return item;
+        }
+
+        public int Execute(string query, object? param = null)
+        {
+            //using IDbConnection db = new SqlConnection(_connectionString);
+            var result = _db.Execute(query, param);
+
+            return result;
+        }
+
+        #region Open_Close Principle
+        //  ရှိပြီးသား code ကို modify မလုပ်ရဘူး features အသစ်ထပ်ထည့်နိုင်ရမယ်။
+        #endregion
+    }
+}
